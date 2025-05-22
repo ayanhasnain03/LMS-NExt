ff707bee8e860d77f1f8334c9eca41d148e77117
Ayan <ayanhasnain2572006@gmail.com>
2025-05-22 15:54:57 +0530

    landing & nav & footer

--- client/src/app/page.tsx
+++ client/src/app/page.tsx
@@ -1,8 +1,16 @@
-import { Button } from "@/components/ui/button";
+import { NonDashboardNavbar } from "@/components/NonDashboardNavbar";
+import Landing from "@/app/(nondashboard)/landing/page";
+import Footer from "@/components/Footer";
+
 
 export default function Home() {
   return (
-<Button>Click me</Button>
+    <div className="nondashboard-layout">
+      <NonDashboardNavbar />
+      <main className="nondashboard-layout__main">
+        <Landing />
+      </main>
+<Footer/>
+    </div>
   );
-
 }
---
+++ client/src/components/NonDashboardNavbar.tsx
@@ -0,0 +1,61 @@
+"use client";
+
+
+import { Bell, BookOpen } from "lucide-react";
+import Link from "next/link";
+import React from "react";
+
+export const NonDashboardNavbar = () => {
+
+  return (
+    <nav className="nondashboard-navbar">
+      <div className="nondashboard-navbar__container">
+        <div className="nondashboard-navbar__search">
+          <Link href="/" className="nondashboard-navbar__brand" scroll={false}>
+            Sujo Verse
+          </Link>
+          <div className="flex items-center gap-4">
+            <div className="relative group">
+              <Link
+                href="/search"
+                className="nondashboard-navbar__search-input"
+                scroll={false}
+              >
+                <span className="hidden sm:inline">Search Courses</span>
+                <span className="sm:hidden">Search</span>
+              </Link>
+              <BookOpen
+                className="nondashboard-navbar__search-icon"
+                size={18}
+              />
+            </div>
+          </div>
+        </div>
+        <div className="nondashboard-navbar__actions">
+          <button className="nondashboard-navbar__notification-button">
+            <span className="nondashboard-navbar__notification-indicator"></span>
+            <Bell className="nondashboard-navbar__notification-icon" />
+          </button>
+
+
+            <Link
+              href="/signin"
+              className="nondashboard-navbar__auth-button--login"
+              scroll={false}
+            >
+              Log in
+            </Link>
+            <Link
+              href="/signup"
+              className="nondashboard-navbar__auth-button--signup"
+              scroll={false}
+            >
+              Sign up
+            </Link>
+
+        </div>
+      </div>
+    </nav>
+  );
+};
+
