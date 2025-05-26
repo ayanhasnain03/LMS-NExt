b8e2eb4d1ced7de2dbc023973ee5f27b09bea2e6
Ayan <ayanhasnain2572006@gmail.com>
2025-05-26 14:15:59 +0530

    fix issue

--- server/src/index.ts
+++ server/src/index.ts
@@ -5,21 +5,29 @@ import cors from "cors";
 import helmet from "helmet";
 import morgan from "morgan";
 import * as dynamoose from "dynamoose";
-// ROUTES IMPORTS
+import serverless from "serverless-http";
+import seed from "./seed/seedDynamodb";
+import {
+  clerkMiddleware,
+  createClerkClient,
+  requireAuth,
+} from "@clerk/express";
+/* ROUTE IMPORTS */
+import courseRoutes from "./routes/courseRoutes";
+import userClerkRoutes from "./routes/userClerkRoutes";
 
-import courseRoutes from "./routes/courseRoutes"
+import userCourseProgressRoutes from "./routes/courseRoutes";
 
-
-
-// CONFIGRATIONS
+/* CONFIGURATIONS */
 dotenv.config();
-
-
 const isProduction = process.env.NODE_ENV === "production";
 if (!isProduction) {
   dynamoose.aws.ddb.local();
 }
 
+export const clerkClient = createClerkClient({
+  secretKey: process.env.CLERK_SECRET_KEY,
+});
 
 const app = express();
 app.use(express.json());
@@ -29,13 +37,17 @@ app.use(morgan("common"));
 app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({ extended: false }));
 app.use(cors());
+app.use(clerkMiddleware());
 
-// ROUTES
-app.get("/",(req,res)=>{
- res.send("Hello World")
-})
+/* ROUTES */
+app.get("/", (req, res) => {
+  res.send("Hello World");
+});
 
-app.use("/courses",courseRoutes)
+app.use("/courses", courseRoutes);
+app.use("/users/clerk", requireAuth(), userClerkRoutes);
+app.use("/users/course-progress", requireAuth(), userCourseProgressRoutes);
+
 /* SERVER */
 const port = process.env.PORT || 3000;
 if (!isProduction) {
@@ -43,3 +55,17 @@ if (!isProduction) {
     console.log(`Server running on port ${port}`);
   });
 }
+
+// aws production environment
+const serverlessApp = serverless(app);
+export const handler = async (event: any, context: any) => {
+  if (event.action === "seed") {
+    await seed();
+    return {
+      statusCode: 200,
+      body: JSON.stringify({ message: "Data seeded successfully" }),
+    };
+  } else {
+    return serverlessApp(event, context);
+  }
+};
