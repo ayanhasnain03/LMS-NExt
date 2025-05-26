ccf320b011a2d39ed9c08bb04c39910bbebca880
Ayan <ayanhasnain2572006@gmail.com>
2025-05-26 22:04:37 +0530

    complete payment page

--- server/src/routes/transactionRoutes.ts
+++ server/src/routes/transactionRoutes.ts
@@ -1,9 +1,9 @@
 import express from "express";
 
-import {createStripePaymentIntent, createTransaction} from "../controllers/transactionController";
+import {createStripePaymentIntent, createTransaction, listTransactions} from "../controllers/transactionController";
 
 const router = express.Router();
-
+router.get("/", listTransactions);
 router.post("/stripe/payment-intent",createStripePaymentIntent)
 router.post("/", createTransaction);
 export default router;
